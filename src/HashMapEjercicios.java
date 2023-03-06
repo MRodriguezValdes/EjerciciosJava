124dfb0572acb70344a0eaadd92bbf553d99d6e5
MRodriguezValdes <mrodriguezvaldes@cifpfbmoll.eu>
2023-03-06 11:56:21 +0100

    Mas ejercicios

--- src/HashMapEjercicios.java
+++ src/HashMapEjercicios.java
@@ -1,2 +1,62 @@
 public class HashMapEjercicios {
+     //Ejercio 1
+    //put(k key , V value) Insertar valor
+
+
+
+    //Ejercicio 2
+    //size() Cuantos pares clave valor hay
+
+
+    //Ejercicio 3
+    //destino.putAll(HashMap origen) copiar un diccionario en otro
+
+
+
+    //Ejercicio 4
+    //clear() borrar todos los datos
+
+
+
+
+    //Ejercicio 5
+    //isEmpty() Verficar si tiene elementos
+
+
+    //Ejercicio 6
+    //HashMap <T,T> copia = new HashMap <T,T>(original) Mapa copia
+
+
+    //Ejercicio 7
+    //containsKey(K key) ver si tien un llave especifica.
+
+
+
+    //Ejercicio 8
+    //Esta repetido, es lo mismo que el 7
+
+
+    //Ejercicio 9
+    //entrySet() devuelve un obejeto typo set
+
+
+
+
+    //Ejercicio 10
+    //get(k Key) devuelve el valor para una clave especifica
+
+
+
+    //Ejercicio 11
+    //keySet()  devuelve un objeto tipo set con las llaves
+
+
+
+    //Ejercicio 12
+    //values() devuelve los valores del hashmap
+
+
+
+    //De forma resumida esta seria la respuesta a los ejercicios.
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,13 @@ import java.util.Collections;
 
 public class Main extends ArraysEjercicios {
     public  void start (){
-    
+        String s1 = "Java";
+        String s2 ="Jac";
+        ArrayList<Integer> numeros=new ArrayList<Integer>();
+        Collections.addAll(numeros,1,2,3,4,5);
+        ArrayList<Integer> numeros2=new ArrayList<>();
+        Collections.addAll(numeros2,1,2,3,4,5);
+        mostrarElementos(numeros);
 
     }
 
